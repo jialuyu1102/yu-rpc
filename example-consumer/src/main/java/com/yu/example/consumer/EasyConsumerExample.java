03717ea845373608a4fb7eabcf6efed653c0d7c1
jialuyu1102 <jialuyu1102@gmail.com>
2024-05-27 10:42:11 +0800

    服务消费模块：pom依赖、服务消费类

---
+++ example-consumer/src/main/java/com/yu/example/consumer/EasyConsumerExample.java
@@ -0,0 +1,23 @@
+package com.yu.example.consumer;
+
+import com.yu.example.common.model.User;
+import com.yu.example.common.service.UserService;
+
+/**
+ * 简单服务消费者示例
+ */
+public class EasyConsumerExample {
+    public static void main(String[] args) {
+        //todo 需要获取UserService 的实现对象
+        UserService userService = null;
+        User user = new User();
+        user.setName("yu");
+        //调用
+        User newUser = userService.getUser(user);
+        if (newUser!=null) {
+            System.out.println(newUser.getName());
+        }else {
+            System.out.println("user == null");
+        }
+    }
+}
