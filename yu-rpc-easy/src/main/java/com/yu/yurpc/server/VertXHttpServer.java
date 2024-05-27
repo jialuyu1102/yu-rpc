60e290205b01e2a78418218e8a021e31cf66f28a
jialuyu1102 <jialuyu1102@gmail.com>
2024-05-27 12:37:29 +0800

    配置请求处理器

--- yu-rpc-easy/src/main/java/com/yu/yurpc/server/VertXHttpServer.java
+++ yu-rpc-easy/src/main/java/com/yu/yurpc/server/VertXHttpServer.java
@@ -15,15 +15,16 @@ public class VertXHttpServer implements HttpServer {
         io.vertx.core.http.HttpServer server = vertx.createHttpServer();
 
         //监听端口 并处理请求
-        server.requestHandler(request -> {
-            //处理 HTTP请求
-            System.out.println("接受到请求 :" + request.method() + "--" + request.uri());
-
-            // 发送HTTP响应
-            request.response()
-                    .putHeader("content-type", "text/plain")
-                    .end("Hello from Vert.x HTTP Server");
-        });
+        server.requestHandler(new HttpServerHandler());
+//        server.requestHandler(request -> {
+//            //处理 HTTP请求
+//            System.out.println("接受到请求 :" + request.method() + "--" + request.uri());
+//
+//            // 发送HTTP响应
+//            request.response()
+//                    .putHeader("content-type", "text/plain")
+//                    .end("Hello from Vert.x HTTP Server");
+//        });
         //启动 HTTP 服务器并监听指定端口
         server.listen(port,result->{
             if (result.succeeded()){
