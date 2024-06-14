899ca3886bf4cc6bb10dc2cca51410d450ff98fa
jly <jialuyu1102@gmail.com>
2024-06-14 22:21:29 +0800

    准备消息结构和网络传输

---
+++ yu-rpc-core/src/main/java/com/yu/yurpc/protocol/ProcotolMessageSerializerEnum.java
@@ -0,0 +1,66 @@
+package com.yu.yurpc.protocol;
+
+import cn.hutool.core.util.ObjectUtil;
+import lombok.Getter;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * 协议消息的序列化器枚举
+ */
+@Getter
+public enum ProcotolMessageSerializerEnum {
+    JDK(0,"jdk"),
+    JSON(0,"json"),
+    KYRO(0,"kyro"),
+    HESSIAN(0,"hessian");
+
+    private final int key;
+
+    private final String value;
+
+    ProcotolMessageSerializerEnum(int key, String value) {
+        this.key = key;
+        this.value = value;
+    }
+
+    /**
+     * 获取值列表
+     * @return
+     */
+    public static List<String> getValues(){
+        return Arrays.stream(values()).map(item->item.value).collect(Collectors.toList());
+    }
+
+    /**
+     * 根据 key 获取枚举
+     */
+    public static ProcotolMessageSerializerEnum getEnumByKey(int key){
+        for (ProcotolMessageSerializerEnum anEnum : ProcotolMessageSerializerEnum.values()) {
+            if (anEnum.key == key){
+                return anEnum;
+            }
+        }
+        return null;
+    }
+
+    /**
+     * 根据value 获取枚举
+     * @param value
+     * @return
+     */
+    public static ProcotolMessageSerializerEnum getEnumByValue(String value){
+        if (ObjectUtil.isEmpty(value)){
+            return null;
+        }
+        for (ProcotolMessageSerializerEnum anEnum : ProcotolMessageSerializerEnum.values()) {
+            if (anEnum.value == value){
+                return anEnum;
+            }
+        }
+        return null;
+    }
+
+}
