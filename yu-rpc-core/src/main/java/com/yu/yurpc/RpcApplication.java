e291d1d7bb502e0c5686e7655f182e9033d08449
jly <jialuyu1102@gmail.com>
2024-06-14 08:18:11 +0800

    支持配置和扩展注册中心

--- yu-rpc-core/src/main/java/com/yu/yurpc/RpcApplication.java
+++ yu-rpc-core/src/main/java/com/yu/yurpc/RpcApplication.java
@@ -1,7 +1,10 @@
 package com.yu.yurpc;
 
+import com.yu.yurpc.config.RegistryConfig;
 import com.yu.yurpc.config.RpcConfig;
 import com.yu.yurpc.constant.RpcConstant;
+import com.yu.yurpc.registry.Registry;
+import com.yu.yurpc.registry.RegistryFactory;
 import com.yu.yurpc.utils.ConfigUtils;
 import lombok.extern.slf4j.Slf4j;
 
@@ -21,6 +24,11 @@ public class RpcApplication {
     public static void init(RpcConfig newRpcConfig) {
         rpcConfig = newRpcConfig;
         log.info("rpc init,config = {}", newRpcConfig.toString());
+        //注册中心初始化
+        RegistryConfig registryConfig = rpcConfig.getRegistryConfig();
+        Registry registry = RegistryFactory.getInstance(registryConfig.getRegistry());
+        registry.init(registryConfig);
+        log.info("registry init,config = {}", registryConfig);
     }
 
     /**
---
+++ yu-rpc-core/src/main/java/com/yu/yurpc/registry/RegistryKeys.java
@@ -0,0 +1,9 @@
+package com.yu.yurpc.registry;
+
+/**
+ * 注册中心 键名 常量
+ */
+public interface RegistryKeys {
+    String ETCD = "etcd";
+    String ZOOKEEPER = "zookeeper";
+}
