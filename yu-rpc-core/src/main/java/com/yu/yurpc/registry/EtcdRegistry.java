3cfb00c711126ea942bc038d4133d97671f194eb
jly <jialuyu1102@gmail.com>
2024-06-14 19:32:15 +0800

    心跳检测

--- yu-rpc-core/src/main/java/com/yu/yurpc/registry/EtcdRegistry.java
+++ yu-rpc-core/src/main/java/com/yu/yurpc/registry/EtcdRegistry.java
@@ -1,5 +1,8 @@
 package com.yu.yurpc.registry;
 
+import cn.hutool.core.collection.CollUtil;
+import cn.hutool.cron.CronUtil;
+import cn.hutool.cron.task.Task;
 import cn.hutool.json.JSONUtil;
 import com.yu.yurpc.config.RegistryConfig;
 import com.yu.yurpc.model.ServiceMetaInfo;
@@ -11,7 +14,9 @@ import io.etcd.jetcd.options.PutOption;
 
 import java.nio.charset.StandardCharsets;
 import java.time.Duration;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
 import java.util.stream.Collectors;
@@ -19,7 +24,7 @@ import java.util.stream.Collectors;
 /**
  * Etcd 注册中心 实现
  */
-public class EtcdRegistry implements Registry{
+public class EtcdRegistry implements Registry {
 
     private Client client;
     private KV kvClient;
@@ -29,6 +34,14 @@ public class EtcdRegistry implements Registry{
      * 路径/rpc/用于区分不同项目
      */
     private static final String ETCD_ROOT_PATH = "/rpc/";
+
+    /**
+     * 本机注册的节点key集合 （用于维护续期）
+     *
+     * @param registryConfig
+     */
+    private final Set<String> localRegistryNodeKeySet = new HashSet<>();
+
     @Override
     public void init(RegistryConfig registryConfig) {
         client = Client.builder()
@@ -36,48 +49,56 @@ public class EtcdRegistry implements Registry{
                 .connectTimeout(Duration.ofMillis(registryConfig.getTimeout()))
                 .build();
         kvClient = client.getKVClient();
+        heartBeat();
     }
 
     @Override
     public void register(ServiceMetaInfo serviceMetaInfo) throws Exception {
-        //创建 Lease 客户端
+        // 创建 Lease 客户端
         Lease leaseClient = client.getLeaseClient();
-        //创建一个30秒的租约
+        // 创建一个30秒的租约
         long leaseId = leaseClient.grant(30).get().getID();
-        //设置要存储的键值对
+        // 设置要存储的键值对
         String registryKey = ETCD_ROOT_PATH + serviceMetaInfo.getServiceNodeKey();
         ByteSequence key = ByteSequence.from(registryKey, StandardCharsets.UTF_8);
-        ByteSequence value = ByteSequence.from(JSONUtil.toJsonStr(serviceMetaInfo),StandardCharsets.UTF_8);
-        //将键值对与租约关联，并设置过期时间
+        ByteSequence value = ByteSequence.from(JSONUtil.toJsonStr(serviceMetaInfo), StandardCharsets.UTF_8);
+        // 将键值对与租约关联，并设置过期时间
         PutOption putOption = PutOption.builder().withLeaseId(leaseId).build();
-        kvClient.put(key,value,putOption).get();
+        kvClient.put(key, value, putOption).get();
+
+        // 添加节点信息到本地缓存
+        localRegistryNodeKeySet.add(registryKey);
+
     }
 
     @Override
     public void unRegister(ServiceMetaInfo serviceMetaInfo) throws Exception {
-        String registerKey  = ETCD_ROOT_PATH + serviceMetaInfo.getServiceNodeKey();
+        String registerKey = ETCD_ROOT_PATH + serviceMetaInfo.getServiceNodeKey();
         kvClient.delete(ByteSequence.from(registerKey, StandardCharsets.UTF_8)).get();
+
+        // 移除本地缓存中的注册信息
+        localRegistryNodeKeySet.remove(registerKey);
     }
 
     @Override
     public List<ServiceMetaInfo> serviceDiscovery(String serviceKey) {
-        //前缀搜索，结尾要加 “/”
+        // 前缀搜索，结尾要加 “/”
         String searchPrefix = ETCD_ROOT_PATH + serviceKey + "/";
         try {
-            //前缀查询
+            // 前缀查询
             GetOption getOption = GetOption.builder().isPrefix(true).build();
             List<KeyValue> keyValues = kvClient.get(
                             ByteSequence.from(searchPrefix, StandardCharsets.UTF_8),
                             getOption)
                     .get().getKvs();
-            //解析服务信息
+            // 解析服务信息
             return keyValues.stream()
                     .map(keyValue -> {
                         String value = keyValue.getValue().toString(StandardCharsets.UTF_8);
                         return JSONUtil.toBean(value, ServiceMetaInfo.class);
                     }).collect(Collectors.toList());
-        }catch (Exception e){
-            throw new RuntimeException("获取服务列表失败",e);
+        } catch (Exception e) {
+            throw new RuntimeException("获取服务列表失败", e);
         }
 
     }
@@ -85,12 +106,43 @@ public class EtcdRegistry implements Registry{
     @Override
     public void destroy() {
         System.out.println("当前节点下线");
-        //释放资源
-        if (kvClient != null){
+        // 释放资源
+        if (kvClient != null) {
             kvClient.close();
         }
-        if (client != null){
+        if (client != null) {
             client.close();
         }
+    }
+
+    @Override
+    public void heartBeat() {
+        // 10秒 续期一次
+        CronUtil.schedule("*/10 * * * * *", new Task() {
+            @Override
+            public void execute() {
+                // 遍历本节点的所有key
+                for (String key : localRegistryNodeKeySet) {
+                    try {
+                        List<KeyValue> keyValues = kvClient.get(ByteSequence.from(key, StandardCharsets.UTF_8)).get().getKvs();
+                        // 若服务列表为 null ，则节点已下线，需重启节点
+                        if (CollUtil.isEmpty(keyValues)) {
+                            continue;
+                        }
+                        // 节点未过期，重新注册（相当于续期）
+                        KeyValue keyValue = keyValues.get(0);
+                        String value = keyValue.getValue().toString(StandardCharsets.UTF_8);
+                        ServiceMetaInfo serviceMetaInfo = JSONUtil.toBean(value, ServiceMetaInfo.class);
+                        register(serviceMetaInfo);
+
+                    } catch (Exception e) {
+                        throw new RuntimeException(key + "续签失败" + e);
+                    }
+                }
+            }
+        });
+        //支持秒级别定时任务
+        CronUtil.setMatchSecond(true);
+        CronUtil.start();
     }
 }
--- yu-rpc-core/src/main/java/com/yu/yurpc/registry/Registry.java
+++ yu-rpc-core/src/main/java/com/yu/yurpc/registry/Registry.java
@@ -40,4 +40,9 @@ public interface Registry {
      */
     void destroy();
 
+    /**
+     * 心跳检测（服务端）
+     */
+    void heartBeat();
+
 }
